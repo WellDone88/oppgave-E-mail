7b3c830f95a3ac50d5ba6525e9230da464a7791d
WellDone88 <kenneth.moren@live.no>
2025-10-01 15:59:23 +0200

    Update view.js

--- view.js
+++ view.js
@@ -1,7 +1,11 @@
 
 function updateview(){
     let HTML = document.getElementById("app").innerHTML = /*HTML*/ `
-    
+    <h1>Panda express</h1>
+    <input type="email"> epost <br>
+    <input type="password"> passord<br>
+    <br>
+    <button>Log inn</button>
     
     
     
